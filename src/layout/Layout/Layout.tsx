3015381672851d5f21846188ba63d5b6a6f8e881
Фёдор Полевой <polevoy@ligat.su>
2025-07-28 09:39:42 +0300

    navLink

--- src/layout/Layout/Layout.tsx
+++ src/layout/Layout/Layout.tsx
@@ -1,15 +1,10 @@
-import { Link, Outlet, useLocation } from 'react-router-dom'
+import { Link, NavLink, Outlet } from 'react-router-dom'
 import styles from './Layout.module.css'
 import buttonStyles from '../../components/Button/Button.module.css'
 import Button from '../../components/Button/Button'
-import { useEffect } from 'react'
 import cn from 'classnames'
 
 export function Layout() {
-	const location = useLocation()
-
-	useEffect(() => console.log(location), [location])
-
 	return (
 		<div className={styles['layout']}>
 			<div className={styles['sidebar']}>
@@ -23,19 +18,28 @@ export function Layout() {
 					<div className={styles['email']}>alaricode@ya.ru</div>
 				</div>
 				<div className={styles['menu']}>
-					<Link
+					<NavLink // Более удобный способ навигации и подсветки активного класса
 						to='/'
-						className={cn(styles['link'], {
-							[styles.active]: location.pathname === '/',
-						})}
+						className={({ isActive }) =>
+							cn(styles['link'], {
+								[styles.active]: isActive,
+							})
+						}
 					>
 						<img src='/menu-icon.svg' alt='Иконка меню' />
 						Меню
-					</Link>
-					<Link to='/cart' className={styles['link']}>
+					</NavLink>
+					<NavLink
+						to='/cart'
+						className={({ isActive }) =>
+							cn(styles['link'], {
+								[styles.active]: isActive,
+							})
+						}
+					>
 						<img src='/cart-icon.svg' alt='Иконка корзины' />
 						Корзина
-					</Link>
+					</NavLink>
 				</div>
 				<Button className={buttonStyles['exit']}>
 					<img src='/exit-icon.svg' alt='Иконка выхода' />
