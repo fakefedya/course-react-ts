0aeece9357917bbf1c078d14006f4ecad5ba5cea
Fedya <lilfedya@yandex.ru>
2025-07-28 22:48:51 +0300

    Параметрические роуты

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { Menu } from './pages/Menu/Menu.tsx'
 import { Cart } from './pages/Cart/Cart.tsx'
 import { Error } from './pages/Error/Error.tsx'
 import { Layout } from './layout/Layout/Layout.tsx'
+import { Product } from './pages/Product/Product.tsx'
 
 // Как правило, роутер объявляется в main.tsx
 const router = createBrowserRouter([
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
 			{
 				path: '/cart',
 				element: <Cart />,
+			},
+			{
+				path: '/product/:id',
+				element: <Product />,
 			},
 		],
 	},
--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,12 +1,25 @@
 import Heading from '../../components/Heading/Heading'
+import ProductCard from '../../components/ProductCard/ProductCard'
 import Search from '../../components/Search/Search'
 import styles from './Menu.module.css'
 
 export function Menu() {
 	return (
-		<div className={styles['head']}>
-			<Heading>Меню</Heading>
-			<Search placeholder='Введите блюдо или состав' />
-		</div>
+		<>
+			<div className={styles['head']}>
+				<Heading>Меню</Heading>
+				<Search placeholder='Введите блюдо или состав' />
+			</div>
+			<div className={styles['product-list']}>
+				<ProductCard
+					id={1}
+					title='Наслаждение'
+					description='Салями, руккола, помидоры, оливки'
+					rating={4.5}
+					price={300}
+					image='/product-demo.png'
+				/>
+			</div>
+		</>
 	)
 }
