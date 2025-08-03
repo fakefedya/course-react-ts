60727e9641477d2e1b4ff5810afc95472e6d9fcc
Fedya <lilfedya@yandex.ru>
2025-08-03 21:25:06 +0300

    Обработка загрузки и обработка ошибок

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,22 +1,29 @@
 import { useEffect, useState } from 'react'
 import Heading from '../../components/Heading/Heading'
-import ProductCard from '../../components/ProductCard/ProductCard'
 import Search from '../../components/Search/Search'
 import { PREFIX } from '../../helpers/API'
 import type { Product } from '../../interfaces/product.interface'
 import styles from './Menu.module.css'
+import axios, { AxiosError } from 'axios'
+import { MenuList } from './MenuList/MenuList'
 
 export function Menu() {
 	const [products, setProducts] = useState<Product[]>([])
+	const [isLoading, setIsLoading] = useState<boolean>(false)
+	const [error, setError] = useState<string | undefined>()
 
 	const getMenu = async () => {
 		try {
-			const res = await fetch(`${PREFIX}/products`)
-			if (!res.ok) return
-			const data = (await res.json()) as Product[]
+			setIsLoading(true)
+			const { data } = await axios.get<Product[]>(`${PREFIX}/products`)
 			setProducts(data)
+			setIsLoading(false)
 		} catch (e) {
 			console.error(e)
+			if (e instanceof AxiosError) {
+				setError(e.message)
+			}
+			setIsLoading(false)
 			return
 		}
 	}
@@ -32,17 +39,9 @@ export function Menu() {
 				<Search placeholder='Введите блюдо или состав' />
 			</div>
 			<div className={styles['product-list']}>
-				{products.map((product) => (
-					<ProductCard
-						key={product.id}
-						id={product.id}
-						name={product.name}
-						description={product.ingredients.join(' ,')}
-						rating={product.rating}
-						price={product.price}
-						image={product.image}
-					/>
-				))}
+				{error && <>{error}</>}
+				{!isLoading && <MenuList products={products} />}
+				{isLoading && <>Загружаем продукты...</>}
 			</div>
 		</>
 	)
---
+++ src/pages/Menu/MenuList/MenuList.tsx
@@ -0,0 +1,16 @@
+import ProductCard from '../../../components/ProductCard/ProductCard'
+import type { MenuListProps } from './MenuList.props'
+
+export function MenuList({ products }: MenuListProps) {
+	return products.map((product) => (
+		<ProductCard
+			key={product.id}
+			id={product.id}
+			name={product.name}
+			description={product.ingredients.join(' ,')}
+			rating={product.rating}
+			price={product.price}
+			image={product.image}
+		/>
+	))
+}
