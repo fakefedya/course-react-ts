a61bf2ee80da89f2d39fd44737dee8915de29857
Фёдор Полевой <polevoy@ligat.su>
2025-08-13 10:19:48 +0300

    +

--- src/layout/Layout/Layout.tsx
+++ src/layout/Layout/Layout.tsx
@@ -1,10 +1,16 @@
-import { NavLink, Outlet } from 'react-router-dom'
+import { NavLink, Outlet, useNavigate } from 'react-router-dom'
 import styles from './Layout.module.css'
 import buttonStyles from '../../components/Button/Button.module.css'
 import Button from '../../components/Button/Button'
 import cn from 'classnames'
 
 export function Layout() {
+	const navigate = useNavigate()
+	const logout = () => {
+		localStorage.removeItem('jwt')
+		navigate('/auth/login')
+	}
+
 	return (
 		<div className={styles['layout']}>
 			<div className={styles['sidebar']}>
@@ -41,7 +47,7 @@ export function Layout() {
 						Корзина
 					</NavLink>
 				</div>
-				<Button className={buttonStyles['exit']}>
+				<Button className={buttonStyles['exit']} onClick={logout}>
 					<img src='/exit-icon.svg' alt='Иконка выхода' />
 					Выход
 				</Button>
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import Button from '../../components/Button/Button'
 import Heading from '../../components/Heading/Heading'
 import Input from '../../components/Input/Input'
@@ -6,6 +6,7 @@ import styles from './Login.module.css'
 import { useState, type FormEvent } from 'react'
 import axios, { AxiosError } from 'axios'
 import { PREFIX } from '../../helpers/API'
+import type { LoginResponse } from '../../interfaces/auth.interface'
 
 export type LoginForm = {
 	email: {
@@ -18,6 +19,7 @@ export type LoginForm = {
 
 export function Login() {
 	const [error, setError] = useState<string | null>()
+	const navigate = useNavigate()
 
 	const submit = async (e: FormEvent) => {
 		e.preventDefault()
@@ -25,15 +27,17 @@ export function Login() {
 		const target = e.target as typeof e.target & LoginForm
 		const { email, password } = target
 		await sendLogin(email.value, password.value)
+		navigate('/') // Для навигации (перенаправления) после успешной авторизации
 	}
 
 	const sendLogin = async (email: string, password: string) => {
 		try {
-			const { data } = await axios.post(`${PREFIX}/auth/login`, {
+			const { data } = await axios.post<LoginResponse>(`${PREFIX}/auth/login`, {
 				email,
 				password,
 			})
 			console.log(data)
+			localStorage.setItem('jwt', data.access_token)
 		} catch (error) {
 			if (error instanceof AxiosError) {
 				setError(error.response?.data.message)
