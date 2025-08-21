ae9c3d0443c08d32d97e6e466414b4b17be97f72
Фёдор Полевой <polevoy@ligat.su>
2025-08-21 17:31:01 +0300

    Регистрация (в процессе)

--- src/store/user.slice.ts
+++ src/store/user.slice.ts
@@ -11,6 +11,7 @@ export const JWT_PERSISTENT_STATE = 'userData'
 export interface UserState {
 	jwt: string | null
 	loginErrorMessage?: string
+	registerErrorMessage?: string
 	profile?: Profile
 }
 
@@ -30,6 +31,27 @@ export const login = createAsyncThunk(
 				email: params.email,
 				password: params.password,
 			})
+			return data
+		} catch (e) {
+			if (e instanceof AxiosError) {
+				throw new Error(e.response?.data.message)
+			}
+		}
+	}
+)
+
+export const register = createAsyncThunk(
+	'user/register',
+	async (params: { email: string; password: string; name: string }) => {
+		try {
+			const { data } = await axios.post<LoginResponse>(
+				`${PREFIX}/auth/register`,
+				{
+					email: params.email,
+					password: params.password,
+					name: params.name,
+				}
+			)
 			return data
 		} catch (e) {
 			if (e instanceof AxiosError) {
@@ -62,6 +84,9 @@ export const userSlice = createSlice({
 		clearLoginError: (state) => {
 			state.loginErrorMessage = undefined
 		},
+		clearRegisterError: (state) => {
+			state.registerErrorMessage = undefined
+		},
 	},
 	extraReducers: (builder) => {
 		builder.addCase(login.fulfilled, (state, action) => {
@@ -70,6 +95,13 @@ export const userSlice = createSlice({
 		})
 		builder.addCase(login.rejected, (state, action) => {
 			state.loginErrorMessage = action.error.message
+		})
+		builder.addCase(register.fulfilled, (state, action) => {
+			if (!action.payload) return
+			state.jwt = action.payload.access_token
+		})
+		builder.addCase(register.rejected, (state, action) => {
+			state.registerErrorMessage = action.error.message
 		})
 		builder.addCase(getProfile.fulfilled, (state, action) => {
 			state.profile = action.payload
